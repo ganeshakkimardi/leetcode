4cd4898a7d9be34b62df5612e4e8d6e716276e74
Ganesh Akkimardi <your-email@example.com>
2025-11-24 18:29:16 +0530

    Add Two Numbers - ListNode

---
+++ LC2_Add_Two_Numbers.java
@@ -0,0 +1,61 @@
+package practise.src.main.java.leetCode;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LC2_Add_Two_Numbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        System.out.println("list 1");
+        printList(l1);
+        System.out.println("list 2");
+        printList(l2);
+
+        System.out.println("output : ");
+        printList(addTwoNumbers(l1, l2));
+    }
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode temp = res;
+        int total, carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            total = carry;
+
+             if (l1 != null) {
+                 total += l1.val;
+                 l1 = l1.next;
+             }
+             if (l2 != null) {
+                 total += l2.val;
+                 l2 = l2.next;
+             }
+             int num = total % 10;
+             carry = total / 10;
+             temp.next = new ListNode(num);
+             temp = temp.next;
+        }
+        return res.next;
+    }
+
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    
+}
