38eea2083c5831dc198cbeb0420a58eb6fe79066
Ganesh Akkimardi <ganeshakkimardi2000@gmail.com>
2025-11-25 18:26:39 +0530

    Longest Palindrome In Given String

---
+++ LC5_LargestPalindromeInGivenString.java
@@ -0,0 +1,50 @@
+package practise.src.main.java.leetCode;
+
+public class LC5_LargestPalindromeInGivenString {
+    public static void main(String[] args) {
+        String str = "babad";
+        str = largestPalindromeInString(str);
+        System.out.println(str);
+    }
+
+    private static String largestPalindromeInString(String str) {
+        Integer i, low, high;
+        String pa = "", palindrome = "";
+        if (str.length() == 1)
+            return str;
+        else {
+            for (i = 1; i < str.length(); i++) {
+                // palindrome of odd length
+                low = high = i;
+
+                while (str.charAt(low) == str.charAt(high)) {
+                    low--;
+                    high++;
+
+                    if (low < 0 || high == str.length())
+                        break;
+                }
+                pa = str.substring(low + 1, high);
+                if (pa.length() > palindrome.length())
+                    palindrome = pa;
+
+                // palindrome of even length
+                low = i - 1;
+                high = i;
+
+                while (str.charAt(low) == str.charAt(high)) {
+                    low--;
+                    high++;
+
+                    if (low < 0 || high == str.length())
+                        break;
+                }
+                pa = str.substring(low + 1, high);
+                if (pa.length() > palindrome.length())
+                    palindrome = pa;
+            }
+            return palindrome;
+        }
+    }
+
+}
