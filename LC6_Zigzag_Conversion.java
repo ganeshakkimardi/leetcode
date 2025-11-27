2cdb8d71fa22652dae98a528b1edc42f73dbd461
Ganesh Akkimardi <ganeshakkimardi2000@gmail.com>
2025-11-27 07:25:04 +0530

    Zig Zag Conversion

---
+++ LC6_Zigzag_Conversion.java
@@ -0,0 +1,29 @@
+package practise.src.main.java.leetCode;
+
+public class LC6_Zigzag_Conversion {
+    public static void main(String[] args) {
+        Integer numrows = 4;
+        String s = "PAYPALISHIRING";
+        System.out.println(convert(s, numrows));
+    }
+
+    private static String convert(String s, Integer numRows) {
+        String[] ans = new String[numRows];
+        for (int i = 0; i< numRows; i++)
+            ans[i] = "";
+
+        int i=0;
+        while(i < s.length()) {
+            for(int index = 0; index < numRows && i< s.length(); index++) {
+                ans[index]+= s.charAt(i++);
+            }
+            for (int index = numRows - 2; index > 0 && i < s.length(); index--) {
+                ans[index]+= s.charAt(i++);
+            }
+        }
+        String res = "";
+        for(String str : ans)
+            res += str;
+        return res;
+    }
+}
