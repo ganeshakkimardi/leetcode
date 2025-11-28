2ba56a9f2491421aeeedec3af5a9bc01c69cba10
Ganesh Akkimardi <ganeshakkimardi2000@gmail.com>
2025-11-28 21:13:06 +0530

    Reverse Integer

---
+++ LC7_ReverseInteger.java
@@ -0,0 +1,19 @@
+package practise.src.main.java.leetCode;
+
+public class LC7_ReverseInteger {
+    public static void main(String[] args) {
+        int x = 1534236469;
+        System.out.println(reverseInteger(x));
+    }
+
+    private static int reverseInteger(int x) {
+        int rev = 0;
+        while(x != 0) {
+            if(rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10)
+                return 0;
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return rev;
+    }
+}
