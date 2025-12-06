a362f668d3d62322eb7b217ec19c740b59cd005a
Ganesh V Akkimardi <65479821+ganeshakkimardi@users.noreply.github.com>
2025-12-07 00:55:28 +0530

    Group Anagrams

---
+++ LC49_GroupAnagrams.java
@@ -0,0 +1,24 @@
+package practise.src.main.java.leetCode;
+
+import java.util.*;
+
+public class LC49_GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> out = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if (!out.containsKey(key))
+                out.put(key, new ArrayList<>());
+            out.get(key).add(s);
+        }
+        return new ArrayList<>(out.values());
+    }
+}
