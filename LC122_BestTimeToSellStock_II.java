472f65efc9bf5894380aa41ec91bbe0c12340cb8
Ganesh V Akkimardi <65479821+ganeshakkimardi@users.noreply.github.com>
2025-12-09 13:57:00 +0530

    Best time to sell stocks II

---
+++ LC122_BestTimeToSellStock_II.java
@@ -0,0 +1,34 @@
+//You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+//
+//On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+//
+//Find and return the maximum profit you can achieve.
+//
+//
+//
+//Example 1:
+//
+//Input: prices = [7,1,5,3,6,4]
+//Output: 7
+//Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+//Total profit is 4 + 3 = 7.
+
+package practise.src.main.java.leetCode;
+
+public class LC122_BestTimeToSellStock_II {
+	public static void main(String[] args) {
+		int[] prices = { 7,1,5,3,6,4 };
+		System.out.println(bestTimeToSellStocks(prices));
+	}
+
+	private static Integer bestTimeToSellStocks(int[] prices) {
+		Integer i, profit = 0;
+		for (i = 1; i < prices.length; i++) {
+			if (prices[i] > prices[i - 1])
+				profit = profit + prices[i] - prices[i - 1];
+		}
+		return profit;
+	}
+
+}
