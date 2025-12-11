3094194e8b0732fe5d5922bf6c810396039adf6b
Ganesh V Akkimardi <65479821+ganeshakkimardi@users.noreply.github.com>
2025-12-11 23:04:33 +0530

    Repeated DNA Sequence

---
+++ LC187_Repeated_DNA_Sequences.java
@@ -0,0 +1,25 @@
+package practise.src.main.java.leetCode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LC187_Repeated_DNA_Sequences {
+    public static void main(String[] args) {
+        String s ="AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        System.out.println(findRepeatedDnaSequences(s));
+    }
+
+    private static List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seenSequence = new HashSet<>();
+        Set<String> repeatedSequence = new HashSet<>();
+
+        for (Integer i = 0; i <= s.length()-10; i++) {
+            String subString = s.substring(i, i+10);
+            if (!seenSequence.add(subString))
+                repeatedSequence.add(subString);
+        }
+        return new ArrayList<>(repeatedSequence);
+    }
+}
