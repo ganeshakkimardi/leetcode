f975bc746a185a75f53ab3f94a8c760723da41a7
Ganesh V Akkimardi <65479821+ganeshakkimardi@users.noreply.github.com>
2025-12-14 13:56:58 +0530

    Product Except Itself

---
+++ LC238_ProductExceptItself.java
@@ -0,0 +1,32 @@
+package practise.src.main.java.leetCode;
+
+import java.util.Arrays;
+
+public class LC238_ProductExceptItself {
+    public static void main(String[] args) {
+        int nums[] = {0,1};
+        int output[] = productExceptSelf(nums);
+        Arrays.stream(output).forEach(System.out::print);
+    }
+
+    private static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
+        }
+        return ans;
+    }
+}
