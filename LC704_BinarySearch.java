5a0a54ef01a406320b984ad70543463fcec8e629
Ganesh V Akkimardi <65479821+ganeshakkimardi@users.noreply.github.com>
2025-12-31 05:10:25 +0530

    Binary Search

---
+++ LC704_BinarySearch.java
@@ -0,0 +1,24 @@
+package practise.src.main.java.leetCode;
+
+public class LC704_BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(search(nums, target));
+    }
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
